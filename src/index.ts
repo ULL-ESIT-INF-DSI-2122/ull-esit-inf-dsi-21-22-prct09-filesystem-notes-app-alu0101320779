c79a2d9526869734c65e6bba3505b591f409e082
usuario <alu0101320779@ull.edu.es>
2022-04-27 10:03:30 +0000

    modificacion pe

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,40 @@
+import {watchFile} from 'fs';
+import {spawn} from 'child_process';
+import { argv } from 'process'
+import {readFile} from 'fs';
+
 /**
- * Adds two numbers
- * @param firstNumber Consists of the first numeric operand of the addition
- * @param secondNumber Consists of the second numeric operand of the addition
- * @return The addition of the two numbers `firstNumber` and `secondNumber`
- *
- * Usage:
- * ```typescript
- * add(1, 7) = 8
- * add(1.7, 3.5) = 5.2
- * ```
+ * Lee el fichero introducido
  */
- export function add(firstNumber: number, secondNumber: number) {
-    return firstNumber + secondNumber;
+readFile(argv[1], (err) => {
+  if (err) {
+    console.log('There must be a problem with the file you are trying to read');
   }
+
+  /**
+   * Mira el fichero introducido mientras se esté ejecutando
+   */
+  watchFile(argv[2], (curr, prev) => {
+     
+    const ls = spawn(argv[3], [argv[4], argv[5], argv[2]]);
+
+    //Controlamos posibles errores
+  
+    //Mostramos por pantalla el tamaño del archivo
+    console.log(`File was ${prev.size} bytes before it was modified.`);
+    console.log(`Now file is ${curr.size} bytes.\n`);
+
+    //Mostramos la salida del comando ls -l -h
+    ls.stdout.pipe(process.stdout);
+    let lsOutput = '';
+    ls.stdout.on('data', (piece) => lsOutput += piece);
+
+    //Mostramos infromación sobre el fichero
+    ls.on('close', () => {
+      const lsOutputAsArray = lsOutput.split(/\s+/);
+      console.log(`El fichero helloworld.txt tiene ${lsOutputAsArray[0]} permisos`);
+      console.log(`Propietario del fichero helloworld.txt ${lsOutputAsArray[2]}`);
+      console.log(`Número de enlaces al fichero helloworld.txt ${lsOutputAsArray[1]}\n`);
+    });
+  });
+});
--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,17 +1,51 @@
 import 'mocha';
 import {expect} from 'chai';
-import {add} from '../src/index';
+import {AddNote} from '../src/flags/addNote';
+import {ModifyNote} from '../src/flags/modifyNotes';
+import {RemoveNote} from '../src/flags/removeNote';
+import {ListNote} from '../src/flags/listNote';
+import {ReadNote} from '../src/flags/readNote';
+import {NewUser} from '../src/flags/newUser';
 
-describe('add function tests', () => {
-  it('add(1, 8) returns value 9', () => {
-    expect(add(1, 8)).to.be.equal(9);
+const myUser = new NewUser();
+const myNote = new AddNote();
+const modifyNote = new ModifyNote();
+const removeNote = new RemoveNote();
+const listNotes = new ListNote();
+const readNote = new ReadNote();
+
+describe('NewUser function tests', () => {
+  it('NewUser returns value 9', () => {
+    expect(myUser.addUser()).to.be.equal(9);
   });
+});
 
-  it('add(-1, 8) returns value 7', () => {
-    expect(add(-1, 8)).to.be.equal(7);
+describe('AddNote function tests', () => {
+  it('AddNote returns value 9', () => {
+    expect(myNote.addNote()).to.be.equal(9);
   });
+});
 
-  it('add(1.2, 3.5) returns value 4.7', () => {
-    expect(add(1.2, 3.5)).to.be.equal(4.7);
+describe('ModifyNote function tests', () => {
+  it('ModifyNote returns value 9', () => {
+    expect(modifyNote.modifyNote()).to.be.equal(9);
+  });
+});
+
+describe('RemoveNote function tests', () => {
+  it('RemoveNote returns value 9', () => {
+    expect(removeNote.removeNote()).to.be.equal(9);
+  });
+});
+
+describe('ListNote function tests', () => {
+  it('ListNote returns value 9', () => {
+    expect(listNotes.listNote()).to.be.equal(9);
+  });
+});
+
+describe('ReadNote function tests', () => {
+  it('ReadNote returns value 9', () => {
+    expect(readNote.readNote()).to.be.equal(9);
   });
 });
